42f478b7dcc7e2467feaa7be7712c2fe8201b12a
kitano <beiyelinai01@gmail.com>
2025-02-04 16:32:31 +0900

    ProductSerach完了

---
+++ src/tests/Feature/ProductSearchTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductSearchTest extends TestCase
+{
+    use RefreshDatabase;
+
+    // 商品名で部分一致検索ができるか
+    public function test_product_name_search()
+    {
+        $user = User::factory()->create();
+
+        Product::create([
+            'name' => 'デジタルカメラ',
+            'brand_name' => 'ブランドA',
+            'description' => '高画質なデジタルカメラ',
+            'price' => 50000.00,
+            'image' => 'camera.jpg',
+            'status' => '新商品',
+            'user_id' => $user->id,
+            'is_sold' => false,
+        ]);
+
+        $response = $this->get('/?search=デジタルカメラ');
+
+        $response->assertStatus(200);
+        $response->assertSee('デジタルカメラ');
+    }
+
+    // 検索状態がマイリストでも保持されているか
+    public function test_product_name_search_with_mylist_page()
+    {
+        $user = User::factory()->create();
+
+        Product::create([
+            'name' => 'デジタルカメラ',
+            'brand_name' => 'ブランドA',
+            'description' => '高画質なデジタルカメラ',
+            'price' => 50000.00,
+            'image' => 'camera.jpg',
+            'status' => '新商品',
+            'user_id' => $user->id,
+            'is_sold' => false,
+        ]);
+
+        $this->get('/?search=デジタルカメラ');
+
+        $response = $this->get('/?page=mylist&search=デジタルカメラ');
+
+        $response->assertStatus(200);
+        $response->assertSee('デジタルカメラ');
+    }
+}
