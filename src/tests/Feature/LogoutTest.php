ba69a84a6e4c522ffbcd4595d0baae90ca1088c4
kitano <beiyelinai01@gmail.com>
2025-02-04 13:50:48 +0900

    LogoutTest完了

---
+++ src/tests/Feature/LogoutTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class LogoutTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    use RefreshDatabase;
+
+    public function testUserCanLogout()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password123'),
+        ]);
+
+        $this->post('/login', [
+            'email' => 'test@example.com',
+            'password' => 'password123',
+        ]);
+
+        $response = $this->post('/logout');
+
+        $response->assertRedirect('/');
+
+        $this->assertGuest();
+
+    }
+}
