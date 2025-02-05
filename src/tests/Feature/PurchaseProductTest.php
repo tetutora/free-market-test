4c04f54b3679cf022267b6737a39f5a369660568
kitano <beiyelinai01@gmail.com>
2025-02-05 14:09:19 +0900

    PuchaseProductTest完了

--- src/tests/Feature/PurchaseProductTest.php
+++ src/tests/Feature/PurchaseProductTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Product;
+use App\Models\Purchase;
 
 class PurchaseProductTest extends TestCase
 {
@@ -32,5 +33,44 @@ class PurchaseProductTest extends TestCase
         $response = $this->post("/purchase/{$product->id}/success");
 
         $response->assertStatus(302);
+    }
+
+    // 購入した商品が商品一覧で「Sold Out」と表示されるか
+    public function test_sold_label()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $product = Product::factory()->create();
+
+        $purchase = \App\Models\Purchase::create([
+            'user_id' => $user->id,
+            'product_id' => $product->id,
+        ]);
+
+        $response = $this->get('/');
+
+        $response->assertSee('Sold Out');
+    }
+
+    // 購入した商品がプロフィールの購入した商品一覧に追加されているか
+    public function test_selling_products()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $product = Product::factory()->create([
+            'user_id' => $user->id,
+            'is_sold' => false
+        ]);
+
+        $purchase = \App\Models\Purchase::create([
+            'user_id' => $user->id,
+            'product_id' => $product->id,
+        ]);
+
+        $response = $this->get('/?page=mylist&search=');
+
+        $response->assertSee($product->name);
     }
 }
